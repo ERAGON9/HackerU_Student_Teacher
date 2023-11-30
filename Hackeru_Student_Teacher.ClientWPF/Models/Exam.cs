400c4ffa54160cd32a1a5e99baac8dfba77c578d
Oz Ifrache <oz99live@gmail.com>
2023-11-30 21:11:31 +0200

    exam

---
+++ Hackeru_Student_Teacher.ClientWPF/Models/Exam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackeru_Student_Teacher.ClientWPF.Models
+{
+    internal class Exam
+    {
+    }
+}
